3db4a5c35bf332db9afb5bd4551b0ea978dabc38
trulli90 <trulli90@gmail.com>
2021-01-23 22:53:26 +0100

    changed card opening and sliding effect

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ const questions = [
 	}, {
 		id: 3,
 		question: 'How does the internet work?'
+	}, {
+		id: 4,
+		question: 'What was the most challenging project in your career?'
+	}, {
+		id: 5,
+		question: 'What would you change in our company?'
+	}, {
+		id: 6,
+		question: 'How does the internet work?'
 	}
 ];
 
@@ -15,43 +24,55 @@ const nav = document.querySelector('nav');
 const restart = document.querySelector('#restart');
 const button = document.querySelector('button');
 const spinner = document.querySelector('#spinner');
-const deck = document.querySelector("#deck");
+const cardDeck = document.querySelector('#card-deck')
 let i = 0;
 
-const turn = () => {
-	const text = document.querySelector(".text");
-	const card = document.querySelector("#card");
-	const turnCard = document.querySelector('#turn-card')
-
+const createCard = () => {
+	const flipCard = document.createElement("div");
+	flipCard.setAttribute("class", "flip-card");
+	flipCard.setAttribute("id", `card${i}`);
+	const flipCardInner = document.createElement("div");
+	flipCardInner.setAttribute("class", "flip-card-inner");
+	flipCardInner.setAttribute("id", `inner${questions[i].id}`);
+	const flipCardFront = document.createElement("div");
+	flipCardFront.setAttribute("class", "flip-card-front");
+	const flipCardBack = document.createElement("div");
+	flipCardBack.setAttribute("class", "flip-card-back");
+	const text = document.createElement("p");
+	text.setAttribute("class", "text");
 	text.textContent = questions[i].question;
-	turnCard.style.transform = "rotateY(180deg)";
 
-	card.innerHTML = "";
+	flipCardBack.appendChild(text);
+	flipCardInner.appendChild(flipCardFront);
+	flipCardInner.appendChild(flipCardBack);
+	flipCard.appendChild(flipCardInner);
+	cardDeck.appendChild(flipCard);
 
-	[...deck.children].forEach(element => {
-		card.insertAdjacentElement('beforeEnd', element);
-	});
-	
-	if (i === questions.length-1) {
-		deck.innerHTML = "";
-		deck.style.cursor = "default"
-	} else {
-		deck.innerHTML = `
-			<div class="flip-card-inner" id="turn-card">
-                <div class="flip-card-front" style="cursor:pointer;">
-                </div>
-                <div class="flip-card-back">
-                    <span class="top"><i class="fa fa-gear big"></i><i class="fa fa-gear small"></i></span>
-                    <p class="text"> </p>
-                    <span class="bottom"><i class="fa fa-gear small"></i><i class="fa fa-gear big"></i></span>
-                </div>
-            </div>`;
-	}
-	
-	i++;
+	flipCard.style.zIndex = 1000-i;
 };
 
-deck.addEventListener("click", turn);
+const move = () => {
+	const flipper = document.querySelector(`#card${i}`);
+	const flipCardInner = document.querySelector(`#inner${questions[i].id}`);
+
+	flipCardInner.style.transform = "rotateY(180deg)";
+
+	flipper.animate([
+		{ transform: "translateX(360px)" }
+	], {
+		fill: "forwards",
+		delay: 400,
+		duration: 1000,
+		easing: "ease-in-out"
+	});
+
+	flipper.style.zIndex = 1000+i;
+
+	document.querySelector("#inner1").style.boxShadow = "3px 3px 15px black";
+
+	i++;
+	createCard();
+};
 
 const spin = () => {
 	button.classList.toggle("spin");
@@ -66,24 +87,17 @@ const unspin = () => {
 const openNav = () => {
 	nav.classList.toggle("hidden");
 	button.style.outline = "inherit";
-}
+};
 
 const refresh = () => {
 	nav.classList.toggle("hidden");
 	i = 0;
-	card.innerHTML = '';
-	deck.innerHTML = `
-			<div class="flip-card-inner" id="turn-card">
-                <div class="flip-card-front" style="cursor:pointer;">
-                </div>
-                <div class="flip-card-back">
-                    <span class="top"><i class="fa fa-gear big"></i><i class="fa fa-gear small"></i></span>
-                    <p class="text"> </p>
-                    <span class="bottom"><i class="fa fa-gear small"></i><i class="fa fa-gear big"></i></span>
-                </div>
-            </div>`;
-}
+	cardDeck.innerHTML = "";
+	createCard();
+};
 
+createCard();
+cardDeck.addEventListener("click", move);
 button.addEventListener("mouseenter", spin);
 button.addEventListener("mouseleave", unspin);
 button.addEventListener("click", openNav);
